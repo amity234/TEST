b0e355e557fb4458f733c530636fab219256c5d4
amity234 <harryashutosh729@gmail.com>
2024-10-23 15:40:38 +0530

    Update stop.py

--- SARKARMUSIC/plugins/admins/stop.py
+++ SARKARMUSIC/plugins/admins/stop.py
@@ -18,7 +18,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await YT.stop_stream(chat_id)
+    await Sar.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention),
