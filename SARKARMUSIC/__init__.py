96d7e1c38cfb2597e481ea3254b2e81d3c31e8fd
amity234 <harryashutosh729@gmail.com>
2024-10-23 13:42:32 +0530

    ︻┻┳═सरकार

---
+++ SARKARMUSIC/__init__.py
@@ -0,0 +1,28 @@
+from SARKARMUSIC.core.bot import YT
+from SARKARMUSIC.core.dir import dirr
+from SARKARMUSIC.core.git import git
+from SARKARMUSIC.core.userbot import Userbot
+from SARKARMUSIC.misc import dbb, heroku
+
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = YT()
+api = SafoneAPI()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
