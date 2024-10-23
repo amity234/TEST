e1833ecc026f1c35abe516903b5abcce6b30348b
amity234 <harryashutosh729@gmail.com>
2024-10-23 15:33:49 +0530

    𝙷𝙰𝚁𝚁𝚈 😈

--- SARKARMUSIC/plugins/admins/seek.py
+++ SARKARMUSIC/plugins/admins/seek.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from SARKARMUSIC import YouTube, app
-from SARKARMUSIC.core.call import YT
+from SARKARMUSIC.core.call import Sar
 from SARKARMUSIC.misc import db
 from SARKARMUSIC.utils import AdminRightsCheck, seconds_to_min
 from SARKARMUSIC.utils.inline import close_markup
@@ -56,7 +56,7 @@ async def seek_comm(cli, message: Message, _, chat_id):
     if "index_" in file_path:
         file_path = playing[0]["vidid"]
     try:
-        await YT.seek_stream(
+        await Sar.seek_stream(
             chat_id,
             file_path,
             seconds_to_min(to_seek),
