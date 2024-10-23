561d10d3f82283e91391698ceec4f8cb4bc2837c
amity234 <harryashutosh729@gmail.com>
2024-10-23 16:08:54 +0530

    Update queue.py

--- SARKARMUSIC/plugins/tools/queue.py
+++ SARKARMUSIC/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from SARKARMUSIC import app
 from SARKARMUSIC.misc import db
-from SARKARMUSIC.utils import YTBin, get_channeplayCB, seconds_to_min
+from SARKARMUSIC.utils import SarBin, get_channeplayCB, seconds_to_min
 from SARKARMUSIC.utils.database import get_cmode, is_active_chat, is_music_playing
 from SARKARMUSIC.utils.decorators.language import language, languageCB
 from SARKARMUSIC.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await YTBin(msg)
+        link = await SarBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
